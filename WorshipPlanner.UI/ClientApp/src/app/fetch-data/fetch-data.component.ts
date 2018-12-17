8b4b5c49685cf92efd77398111ba51b314ce3660
thedylanyoung <dylan.young.1994@gmail.com>
2018-12-17 12:07:30 -0500

    npm install and updated fetch-data

--- WorshipPlanner.UI/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ WorshipPlanner.UI/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -6,18 +6,11 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './fetch-data.component.html'
 })
 export class FetchDataComponent {
-  public forecasts: WeatherForecast[];
+  public data: string[];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'api/SampleData/WeatherForecasts').subscribe(result => {
-      this.forecasts = result;
+    http.get<string[]>(baseUrl + 'api/values').subscribe(result => {
+      this.data = result;
     }, error => console.error(error));
   }
-}
-
-interface WeatherForecast {
-  dateFormatted: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
 }
